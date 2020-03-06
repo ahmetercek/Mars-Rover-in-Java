eea653fda0f314d0067337acfc6d5e264f74ca4b
okuyucuarif <arifokuyucu0909@hotmail.com>
2020-03-06 21:41:42 +0300

    Add files via upload

---
+++ MarsRover/src/Rover.java
@@ -0,0 +1,83 @@
+public class Rover {
+    public static final Integer N = 1;
+    public static final Integer E = 2;
+    public static final Integer S = 3;
+    public static final Integer W = 4;
+
+
+    public Integer x = 0;
+    public Integer y = 0;
+    public Integer route = N;
+
+
+    public Rover(int x, int y, int route) {
+        this.x = x;
+        this.y = y;
+        this.route = route;
+    }
+
+    public void setPosition(int x, int y, int route) {
+        this.x = x;
+        this.y = y;
+        this.route = route;
+
+    }
+
+    public void process(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'L') {
+                turnLeft();
+            } else if (s.charAt(i) == 'R') {
+                turnRight();
+            } else {
+                move();
+            }
+        }
+    }
+
+    public void move() {
+        if (this.route == N) {
+            this.y++;
+        } else if (this.route == E) {
+            this.x++;
+        } else if (this.route == S) {
+            this.y--;
+        } else if (this.route == W) {
+            this.x--;
+        }
+    }
+
+    public void turnRight() {
+        if (this.route + 1 > W) {
+            this.route = N;
+        } else {
+            this.route++;
+        }
+    }
+
+    public void turnLeft() {
+        if (this.route - 1 < N) {
+            this.route = W;
+        } else {
+            this.route = route - 1;
+        }
+    }
+
+
+    public void printOurLocation() {
+        String temp;
+        if (this.route == N) {
+            temp = "N";
+        } else if (this.route == E) {
+            temp = "E";
+        } else if (this.route == S) {
+            temp = "S";
+        } else {
+            temp = "W";
+        }
+
+        System.out.println(x + " " + y + " " + temp);
+    }
+
+
+}
---
+++ MarsRover/src/RoverTest.java
@@ -0,0 +1,17 @@
+public class RoverTest {
+    public static final Integer N = 1;
+    public static final Integer E = 2;
+    public static final Integer S = 3;
+    public static final Integer W = 4;
+
+
+    public static void main(String args[]) {
+        Rover rover = new Rover(0, 0, N);
+        rover.setPosition(1, 2, N);
+        rover.process("LMLMLMLMM");
+        rover.printOurLocation(); // prints 1 3 N
+        rover.setPosition(3, 3, E);
+        rover.process("MMRMMRMRRM");
+        rover.printOurLocation(); // prints 5 1 E
+    }
+}
